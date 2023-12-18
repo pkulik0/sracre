eb6205c781a2de247ae5e8e75b244d0bb36b685c
pkulik0 <pkulik0@icloud.com>
2023-12-18 18:34:03 +0100

    Arg parsing, data loading

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import argparse
+import os
+from PIL import Image
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "webp"]
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="sracre")
+    parser.add_argument("--text", type=str, default="text.txt")
+    parser.add_argument("--images", type=str, default="input")
+    parser.add_argument("--output", type=str, default="output.mp4")
+    parser.add_argument("--fps", type=int, default=30)
+    return parser.parse_args()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def load_data(text_file, images_dir):
+    lines = []
+    with open(text_file, "r") as f:
+        lines_raw = f.readlines()
+        for line in lines_raw:
+            stripped = line.strip()
+            if stripped:
+                lines.append(stripped)
+
+    images = []
+    for i in range(len(lines)):
+        was_found = False
+        for ext in ALLOWED_EXTENSIONS:
+            filename = os.path.join(images_dir, f"{i}.{ext}")
+            if os.path.exists(filename):
+                was_found = True
+                images.append(Image.open(filename))
+                break
+        if not was_found:
+            raise FileNotFoundError(f"Image {i} was not found")
+    return lines, images
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    args = parse_args()
+    lines, images = load_data(args.text, args.images)
+    print(lines)
+    print(images)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    main()
