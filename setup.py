d693ff448c13e892b62b7f48aae17ab3e85cbdab
pkulik0 <pkulik0@icloud.com>
2023-12-18 23:52:21 +0100

    Update reqs + add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sracre',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/pkulik0/sracre',
+    license='',
+    author='pkulik',
+    author_email='',
+    description=''
+)
